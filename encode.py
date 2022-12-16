9ed9f5d384c63639eb567be8c73a3b5336dbefd8
Harshita Sinha <73345908+Harshi-itaSinha@users.noreply.github.com>
2022-12-16 16:40:27 +0530

    minor changes

--- encode.py
+++ encode.py
@@ -1,4 +1,4 @@
 import hashlib
-word=input("Enter the word to be encoded:")
+word=input("Enter the word to be encoded: ")
 hashed_word=hashlib.md5(word.encode()).hexdigest()
 print(hashed_word)
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import hashlib
-wlist=input("wordlist: ")
-hash2crack=input("hash: ")
 
-rainbow =open(wlist,"r").readlines()
-#print(wlistlines)
+wlist = input("wordlist: ")
+hash2crack = input("hash: ")
+
+rainbow = open(wlist, "r").readlines()
+# print(wlistlines)
 
 
-for i in range(0,len(rainbow)):
-    hash2comp=hashlib.md5(rainbow[i].replace("\n","").encode()).hexdigest()
+for i in range(0, len(rainbow)):
+    hash2comp = hashlib.md5(rainbow[i].replace("\n", "").encode()).hexdigest()
 
-    if(hash2crack==hash2comp):
-        print("password found: "+rainbow[i].replace("\n"," "))
+    if hash2crack == hash2comp:
+        print("password cracked: " + rainbow[i].replace("\n", " "))
         exit()
 
-
-print("Password Not Found")
+print("Wasn't able to crack the hash password")
